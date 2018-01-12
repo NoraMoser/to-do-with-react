36bb66439c0d8e22ac764410a4f23328db637072
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2018-01-11 20:16:04 -0600

    we have a button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import List from './List/List';
 
 class App extends Component {
+
+  addItemHandler = () => {
+    console.log('button was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>To-Do List</h1>
         <List />
+        <button onClick={this.addItemHandler}>Click Me</button>
       </div>
     );
   }
--- src/List/List.js
+++ src/List/List.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const list = () => {
+
+    
+    
     return <div>
         <input className='input'></input>
+        
         </div>
 };
 
